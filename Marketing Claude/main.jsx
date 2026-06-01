4778d66b49a4c50e08cd3c343b2f90c3f14c9863
lucagiunta-tech <luca.giunta@nassastudio.it>
2026-06-01 11:38:35 +0200

    Add files via upload

--- Marketing Claude/main.jsx
+++ Marketing Claude/main.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./MarketingStudio_refactor_step38.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import MarketingStudio from './MarketingStudio.jsx'
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <MarketingStudio />
+  </StrictMode>
+)
